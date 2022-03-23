71c81be6468b1beffc00888152f2e6d4457792fb
siliconjungle <siliconjungle.io@gmail.com>
2022-03-23 16:12:32 +1100

    add env port

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.use(express.static('public'))
 
